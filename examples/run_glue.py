73a61766129c0c504250a974621dacdbcf415340
shreyancej <shreyancej@gmail.com>
2020-03-16 21:27:16 -0400

    syntax error

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -61,7 +61,7 @@ from transformers import glue_convert_examples_to_features as convert_examples_t
 logger = logging.getLogger(__name__)
 
 ALL_MODELS = sum((tuple(conf.pretrained_config_archive_map.keys()) for conf in (BertConfig, XLNetConfig, XLMConfig, 
-                                                                                RobertaConfig, , AlbertConfig)), ())
+                                                                                RobertaConfig,AlbertConfig)), ())
 
 MODEL_CLASSES = {
     'bert': (BertConfig, BertForSequenceClassification, BertTokenizer),
